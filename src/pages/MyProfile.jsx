c7a3ef698f4e944c17f0f0c3317554adc7d6b84e
kalbek <access2kaleb@gmail.com>
2023-05-18 19:14:59 +0300

    Implement display joined missions inside profile page.

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <>
-    <div className="flex-ccc">
-      <div>MyProfile</div>
-    </div>
-  </>
-);
-
+const MyProfile = () => {
+  const { missions } = useSelector((store) => store.missions);
+  return (
+    <>
+      <div className="flex-centered">
+        <table>
+          <tr className="flex-start p-1">
+            <th className="title-profile flex-start">My Missions</th>
+          </tr>
+          {missions.map(
+            (mission) => mission.reserved && (
+            <div key={mission.mission_id}>
+              <tr className="flex relative">
+                <td className="p-1 w-40 wrap profile">
+                  <p>{mission.mission_name}</p>
+                </td>
+              </tr>
+            </div>
+            ),
+          )}
+        </table>
+        <table>
+          <tr>
+            <th className="title-profile flex-start">My Rockets</th>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+};
 export default MyProfile;
