4a5c51c916ec4c7e0f4e4c253eaba80ce2a48602
kalbek <access2kaleb@gmail.com>
2023-05-18 10:39:56 +0300

    Apply providers for react.

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,41 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 
-const Mission = ({ mission, description, status }) => (
-  <>
-    <section className="missions">
-      <div>
-        <ul className="missionsHead">
-          <li>Mission</li>
-          <li>Description</li>
-          <li>Status</li>
-        </ul>
-      </div>
-      <div>
-        <ul className="missionsIndex">
-          <li>
-            <h2>{mission}</h2>
-          </li>
-          <li>
-            <p>{description}</p>
-          </li>
-          <li>
-            <h3>{status}</h3>
-          </li>
-          <li>
-            <button type="button">Join Mission</button>
-          </li>
-        </ul>
-      </div>
-    </section>
+import { useDispatch } from 'react-redux';
+import { getMissions } from '../redux/missions/missionsSlice';
 
-  </>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+  return (
+    <>
+      <section className="missions">
+        <div />
+        <div>
+          <ul className="missionsIndex">
+            <li>
+              <button type="button">Join Mission</button>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Mission;
-
-Mission.propTypes = {
-  mission: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-};
